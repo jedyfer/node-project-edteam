51342fc15de52a0969ba2385a50d7a2df4073f93
Jediael Fernández <jedyfer21@gmail.com>
2022-10-03 10:52:20 -0500

    8.7 Agregando sockets (en sincronia si un usuario entra despues puede ver el dibujo)

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,18 @@
 
 module.exports = (io) => {
+    data = []
+
     //  console.log('Archivo de sockets')
-    io.on('connection', (socket) => {
+    io.on('connection', (socket) => {   
+        for (let i = 0; i < data.length; i++) {
+            //  enviando cada dibujo para que sea visible por usuarios que recien se conecten
+            io.emit('show_drawing', data[i])
+        }
+
         socket.on('drawing', (drawing) => { //  escuchando el evento del cliente
+            //  almacenando el dibujo dentro del array
+            data.push(drawing)
+
             //  emitiendo el evento show_drawing para los demas usuarios
             io.emit('show_drawing', drawing)
         })
