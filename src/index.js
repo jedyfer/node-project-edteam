24e97ca27bc73385fecb7f57a8831e2cb43a4204
Jediael Fernández <jedyfer21@gmail.com>
2022-10-03 12:48:47 -0500

    Heroku deploy

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const http = require('http')    //  servidor para el socket
 const path = require('path')
 const sockeio = require('socket.io')
 
+//  para que heroku no bloquee el acceso a los sockets
+const cors = require('cors')
+
 //  server con express
 const app = express()
 //  server con http
@@ -12,12 +15,17 @@ const server = http.createServer(app)
 const io = sockeio(server)
 require('./socket')(io)
 
-app.set('port', 3000)
+//  MODIFICAMOS EL PUERTO
+app.set('port', process.env.PORT || 5000)   //  significa que si heroku tiene una var. de entorno que use eso
 
 //  middleware
 app.use(express.static(path.join(__dirname, 'public'))) //  acceso a mis archivos html css js
 
+//  middleware para cors
+app.use(cors())
+
 server.listen(app.get('port'), () => {
     console.log('App corriendo en el puerto ' + app.get('port'))
 })
+
 
